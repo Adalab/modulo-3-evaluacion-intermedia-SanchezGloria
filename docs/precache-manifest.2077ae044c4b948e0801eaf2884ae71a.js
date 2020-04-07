86efc0deef92a3942507013b6658fbb48d0b2f4e
SanchezGloria <gloriasancheztrillo@gmail.com>
2020-04-07 10:13:14 +0200

    Update docs

---
+++ docs/precache-manifest.2077ae044c4b948e0801eaf2884ae71a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4056c679608ee3c9fc4e658e5bd4d010",
+    "url": "./index.html"
+  },
+  {
+    "revision": "86fef3cb693ee8532c04",
+    "url": "./static/css/main.1c9cce6f.chunk.css"
+  },
+  {
+    "revision": "97259cd39bac10eb62e7",
+    "url": "./static/js/2.d4e3521c.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "./static/js/2.d4e3521c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "86fef3cb693ee8532c04",
+    "url": "./static/js/main.17c14613.chunk.js"
+  },
+  {
+    "revision": "79904451c1c9b97f250a",
+    "url": "./static/js/runtime-main.7df81f36.js"
+  }
+]);
