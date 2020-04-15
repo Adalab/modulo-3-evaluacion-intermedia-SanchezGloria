412ead694787b2558c09902d18fd89ca33edec05
SanchezGloria <gloriasancheztrillo@gmail.com>
2020-04-15 11:15:39 +0200

    :rocket: add mark as favourite functionality

--- src/Components/App.js
+++ src/Components/App.js
@@ -8,17 +8,39 @@ class App extends React.Component {
     super(props);
     this.state = {
       data: Data,
+      arrayFavs: [],
+      className: '',
     };
+    this.handleCard = this.handleCard.bind(this);
   }
+
+  handleCard = (id) => {
+    let clickedId = parseInt(id);
+    // console.log(clickedId);
+    let prevfavorites = this.state.arrayFavs;
+    let favIndex = prevfavorites.indexOf(clickedId);
+    if (favIndex === -1) {
+      this.setState((prevState) => {
+        return {
+          arrayFavs: [...prevState.arrayFavs, clickedId],
+        };
+      });
+    } else {
+      prevfavorites.splice(favIndex, 1);
+      this.setState({ arrayFavs: [...prevfavorites] });
+    }
+  };
+
   render() {
     // console.log(this.state.data);
+    // console.log(this.state.arrayFavs);
     return (
       <div className="App">
         <header className="page__header">
           <h1 className="page__header--tittle">Mi lista de Pokemons</h1>
         </header>
         <main className="page__main">
-          <Pokelist pokemons={this.state.data} />
+          <Pokelist pokemons={this.state.data} arrayFavs={this.state.arrayFavs} handleCard={this.handleCard} />
         </main>
       </div>
     );
--- src/Components/Pokelist.js
+++ src/Components/Pokelist.js
@@ -4,13 +4,26 @@ import PropTypes from 'prop-types';
 import '../Stylesheets/Pokelist.css';
 
 class Pokelist extends React.Component {
+  constructor(props) {
+    super(props);
+    this.styleFav = '';
+  }
+  handleCardClick = (ev) => {
+    // console.log(ev.currentTarget.id);
+    this.props.handleCard(ev.currentTarget.id);
+  };
   render() {
     // console.log(this.props.pokemons);
     return (
       <ul className="page__main--list">
         {this.props.pokemons.map((pokemon, index) => {
+          if (this.props.arrayFavs.includes(pokemon.id)) {
+            this.styleFav = 'favorite';
+          } else {
+            this.styleFav = '';
+          }
           return (
-            <li className="page__main--card" key={index} id={pokemon.id}>
+            <li className={`page__main--card ${this.styleFav}`} key={index} id={pokemon.id} onClick={this.handleCardClick}>
               <Pokemon pokemon={pokemon} />
             </li>
           );
